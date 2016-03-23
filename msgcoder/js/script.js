2733fee1fcd1a44bc3d82937ab20131e159714f0
spammailnt4u <spammailnt4u@gmail.com>
2016-03-22 22:32:13 -0400

    Simplified foreach calls for message coder

--- msgcoder/js/script.js
+++ msgcoder/js/script.js
@@ -13,24 +13,24 @@ var cont = true;
 
 function coder() {
   var message = document.getElementById("msg").value.split(" ");
-  message.forEach(msgcoder);
+  message.forEach(function(element, index, array){
+    if (element.length > 10) {
+      if (!confirm(element+" is longer than 10 characters and therefore will not convert correctly. Do you understand this?")) {
+        cont = false;
+        return;
+      }
+    }
+    array[index] = parseInt(element,36);
+  });
   if (cont == true) {document.getElementById("result").value = message}
   cont = true;
 }
 
-function msgcoder(element,index,array) {
-  if (element.length > 10) {
-    if (!confirm(element+" is longer than 10 characters and therefore will not convert correctly. Do you understand this?")) {
-      cont = false;
-      return;
-    }
-  }
-  array[index] = parseInt(element,36)
-}
-
 function decoder() {
   var message2 = document.getElementById("msg2").value.split(",");
-  message2.forEach(msgdecoder);
+  message2.forEach(function(element,index,array){
+    array[index] = Number(element).toString(36);
+  });
   if (message2 == "NaN") {
     alert("Please enter \(a\) number\(s\)");
     return;
@@ -38,8 +38,4 @@ function decoder() {
   else {
     document.getElementById("result2").value = message2.join(" ");
   }
-}
-
-function msgdecoder(element,index,array) {
-  array[index] = Number(element).toString(36);
 }
